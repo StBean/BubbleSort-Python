52065ed457dbc1b3d947df06bea1eb8d77c594f1
Anthony Poblacion <StBean@users.noreply.github.com>
2019-03-22 22:17:04 -0700

    Update bubbleSorty.py

--- bubbleSorty.py
+++ bubbleSorty.py
@@ -1,11 +1,8 @@
-list = [5,6,2,3,0,-3,-4,5,30,45,600,12,7,3,8,5,1,8,3,6,44,5,99]
 def bubbleSort(data):
-	for i in range(len(data)):
-		for i in range(len(data) - 1 - i):
-			if data[i + 1] < data[i]:
-				temp = data[i + 1]
-				data[i + 1] = data[i]
-				data[i] = temp
-
-result = bubbleSort(list)
-print(list)
+	for i in range(0, len(data) - 1, 1):
+		for j in range(i + 1, len(data), 1):
+			if(data[j] < data[i]:
+			   temp = data[j]
+			   data[j] = data[i]
+			   data[i] = temp
+	return data
